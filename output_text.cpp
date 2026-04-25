3c34589e214732bac99ebc62c026a5a6662aabc3
Wong Chun Ming <u3658696@connect.hku.hk>
2026-04-25 23:34:23 +0800

    Fix formatting and punctuation in output_text.cpp

--- output_text.cpp
+++ output_text.cpp
@@ -25,10 +25,10 @@ void writer_print(const string& text, int char_delay, int dot_delay, bool is_dot
         // Get the last character of the string
         if (!text.empty()) {
             char last_char = text.back();
-            if (last_char == '!' || last_char == '?') {
+            if (last_char == '!' || last_char == '?'|| last_char == ')') {
                 this_thread::sleep_for(chrono::milliseconds(200));
                 // Make sure we do not create dots if there are other punctuations
-                skip_dots = true; // skip dots as we do not need tem when there are ! or ?
+                skip_dots = true; // skip dots as we do not need them when there are ! or ?
             }
         }
         if (!skip_dots) {
