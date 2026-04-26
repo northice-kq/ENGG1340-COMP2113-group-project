9272c907011dce98892560ecb389c9c1858d92f9
HansonW0ng <hanson.wrkac@gmail.com>
2026-04-26 20:01:32 +0800

    Update entities.h

--- entities.h
+++ entities.h
@@ -1,44 +1,46 @@
 #ifndef ENTITIES_H
 #define ENTITIES_H
 
+#include "player.h'
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-struct player{
-    int hp;
-    int attack;
-    int killcnt;
-};
-
 //parent class of enemy
 class Enemy{
 public:
+    string name;
     int hp;
     int attack;
     
     virtual ~Enemy();
-    Enemy(int h, int a);
+    Enemy(int h, int a, string name);
     
     void takedamage(int damage);
-    virtual void attackAction();
+    virtual int attackAction();
+    virtual int attackAction(int l);
+    virtual bool attemptDodge() {
+        return false;
+    }
 };
 
 //sub-class Mage
 class Mage : public Enemy{
 public:
     int streak;
-    Mage(int h, int att);
-    void attackAction() override;
+    Mage(int h, int att, string name);
+    int attackAction(int l) override;
 };
 
 //sub-class Mage
 class Assassin : public Enemy{
 public:
     double sneakRate;
-    Assassin(int h, int att);
-    void attackAction() override;
+    bool sneaked;
+    Assassin(int h, int att, string name);
+    int attackAction() override;
+    bool attemptDodge() override;
 };
 
 Enemy* generateEnemy(int killcnt);
