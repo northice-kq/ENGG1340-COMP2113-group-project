adad373a0f5591923be40e34941187d140d4a942
Wong Chun Ming <u3658696@connect.hku.hk>
2026-04-29 20:50:45 +0800

    Refactor writer_print default parameters
    Updated default delays for writer_print function.

--- output_text/output_text.h
+++ output_text/output_text.h
@@ -23,7 +23,7 @@ using namespace std;
 
 //          NOTE!!!! To use the default settings, omit the last three perimeters
 
-void writer_print(const string& text, bool is_dots = true, bool is_new_line = true, int char_delay = 40, int dot_delay = 120);
+void writer_print(const string& text, bool is_dots = true, bool is_new_line = true, int char_delay = 20, int dot_delay = 100);
 
 //
 // What it does: waits for the player to press Enter before continuing.
