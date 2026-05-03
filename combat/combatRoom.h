9fb3259e0985d981333bfa70bf40bd93ea525da8
HansonW0ng <hanson.wrkac@gmail.com>
2026-05-03 17:51:20 +0800

    Update combatRoom.h

--- combat/combatRoom.h
+++ combat/combatRoom.h
@@ -4,6 +4,7 @@
 #include "../player/player.h"
 #include "../entities/entities.h"
 
+//enter a combat room and start a turn based combat, input the address of player, pointer of enemies generated, and the difficulty of the game
 void combatRoom(Player &p1, Enemy* currentEnemy, bool isHard);
 
 #endif
