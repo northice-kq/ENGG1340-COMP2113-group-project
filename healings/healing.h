e0ed390ccec694a78d8658991b8f245b158ceb39
leo-060 <112762440+leo-060@users.noreply.github.com>
2026-05-03 18:04:12 +0800

    Add comments to functions

--- healings/healing.h
+++ healings/healing.h
@@ -6,9 +6,13 @@
 struct Healing {
     std::string name;
     std::string description;
-    Healing(std::string name, std::string description);
+    Healing(std::string name, std::string description); // constructor
     bool healPlayer(int& HP, int maxHP);
+    // call to heal the player (change the player HP in place)
+    // return whether the operation is successful or not
     virtual int getFinalHP(int HP, int maxHP) const = 0;
+    // helper function to get the final HP of a player after healing is used
+    // returns -1 if the healing cannot heal the player further
 };
 
 struct firstAidKit : Healing {
